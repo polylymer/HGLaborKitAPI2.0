41d136ff94a289a410c937f83e5e75a8bc8faec2
mooziii <63669478+mooz-lego@users.noreply.github.com>
2021-01-25 14:40:58 +0100

    Add Tank Kit

---
+++ src/main/java/de/hglabor/plugins/kitapi/kit/kits/TankKit.java
@@ -0,0 +1,54 @@
+package de.hglabor.plugins.kitapi.kit.kits;
+
+import de.hglabor.plugins.kitapi.kit.AbstractKit;
+import de.hglabor.plugins.kitapi.kit.config.KitSettings;
+import org.bukkit.Material;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.CraftItemEvent;
+
+public class TankKit extends AbstractKit {
+    public static final TankKit INSTANCE = new TankKit();
+    protected TankKit() {
+        super("Tank", Material.TNT);
+        addSetting(KitSettings.EXPLOSION_SIZE_PLAYER, 6);
+        addSetting(KitSettings.EXPLOSION_SIZE_ENTITY, 1);
+        addSetting(KitSettings.EXPLOSION_SIZE_RECRAFT, 1);
+    }
+
+
+
+    @Override
+    public void onEntityDeath(EntityDeathEvent event) {
+        LivingEntity entity = event.getEntity();
+        int explosionSize = entity instanceof Player ? (Integer) getSetting(KitSettings.EXPLOSION_SIZE_PLAYER) : (Integer) getSetting(KitSettings.EXPLOSION_SIZE_ENTITY);
+        entity.getWorld().createExplosion(entity.getLocation(), explosionSize, false, true);
+    }
+
+    @Override
+    public void onEntityDamage(EntityDamageEvent event) {
+        if (event.getCause() == EntityDamageEvent.DamageCause.ENTITY_EXPLOSION || event.getCause() == EntityDamageEvent.DamageCause.BLOCK_EXPLOSION) {
+            event.setCancelled(true);
+        }
+    }
+
+    @Override
+    public void onCraftItem(CraftItemEvent event) {
+        Player player = (Player) event.getInventory().getViewers().get(0);
+        if (!event.getRecipe().getResult().getType().equals(Material.MUSHROOM_STEW)) {
+            return;
+        }
+
+        if (!(event.getClick() == ClickType.SHIFT_LEFT || event.getClick() == ClickType.SHIFT_RIGHT)) {
+            return;
+        }
+
+        if (player.getInventory().firstEmpty() != -1) {
+            player.getWorld().createExplosion(player.getLocation(), (Integer) getSetting(KitSettings.EXPLOSION_SIZE_RECRAFT), false, true, player);
+        }
+
+    }
+}
