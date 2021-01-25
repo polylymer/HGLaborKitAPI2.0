443a7769852bc79b3dc4439f7c41724562829e87
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2021-01-25 14:59:25 +0100

    Fixxed missing cannibal multiplier value

--- src/main/java/de/hglabor/plugins/kitapi/kit/kits/CannibalKit.java
+++ src/main/java/de/hglabor/plugins/kitapi/kit/kits/CannibalKit.java
@@ -17,6 +17,7 @@ public class CannibalKit extends AbstractKit {
     private CannibalKit() {
         super("Cannibal", Material.TROPICAL_FISH);
         addSetting(KitSettings.EFFECT_DURATION,2);
+        addSetting(KitSettings.EFFECT_MULTIPLIER, 1);
         addSetting(KitSettings.LIKELIHOOD,33);
     }
 
@@ -40,7 +41,7 @@ public class CannibalKit extends AbstractKit {
             attack.setFoodLevel(foodLevelOfPlayer + difference);
         }
 
-        if (ChanceUtils.roll((Integer) getSetting(KitSettings.LIKELIHOOD))) {
+        if (ChanceUtils.roll(getSetting(KitSettings.LIKELIHOOD))) {
             enemy.addPotionEffect(new PotionEffect(PotionEffectType.HUNGER, (Integer) getSetting(KitSettings.EFFECT_DURATION) * 20, (Integer) getSetting(KitSettings.EFFECT_MULTIPLIER)));
         }
     }
