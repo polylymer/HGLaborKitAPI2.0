c50622bdd37e4229f72905a2140f8a19d57bfa9c
mooziii <63669478+mooz-lego@users.noreply.github.com>
2021-01-25 14:41:07 +0100

    Add GravityKit

---
+++ src/main/java/de/hglabor/plugins/kitapi/kit/kits/GravityKit.java
@@ -0,0 +1,46 @@
+package de.hglabor.plugins.kitapi.kit.kits;
+
+import de.hglabor.plugins.kitapi.kit.AbstractKit;
+import de.hglabor.plugins.kitapi.kit.KitManager;
+import de.hglabor.plugins.kitapi.kit.config.KitSettings;
+import de.hglabor.plugins.kitapi.player.KitPlayer;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class GravityKit extends AbstractKit {
+    public static final GravityKit INSTANCE = new GravityKit();
+    protected GravityKit() {
+        super("Gravity", Material.MAGENTA_GLAZED_TERRACOTTA, 30);
+        setMainKitItem(getDisplayMaterial(), 1);
+        addSetting(KitSettings.EFFECT_MULTIPLIER, 3);
+        addSetting(KitSettings.EFFECT_DURATION, 1);
+        addSetting(KitSettings.USES, 3);
+    }
+
+    @Override
+    public void disable(KitPlayer kitPlayer) {
+        Player player = Bukkit.getPlayer(kitPlayer.getUUID());
+        if (player.hasPotionEffect(PotionEffectType.LEVITATION)) {
+            player.removePotionEffect(PotionEffectType.LEVITATION);
+        }
+    }
+
+    @Override
+    public void onPlayerRightClickKitItem(PlayerInteractEvent event) {
+        KitPlayer kitPlayer = KitManager.getInstance().getPlayer(event.getPlayer());
+        Player player = event.getPlayer();
+
+        if (player.hasPotionEffect(PotionEffectType.LEVITATION)) {
+            kitPlayer.activateKitCooldown(this, this.getCooldown());
+            player.removePotionEffect(PotionEffectType.LEVITATION);
+        } else {
+            player.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, Integer.MAX_VALUE, (Integer) getSetting(KitSettings.EFFECT_MULTIPLIER)));
+        }
+    }
+    //TODO: Add PlayerHitEntityWithKitItem Event
+
+}
