f21a1a4432a60bec9ac3b909fdd6ed20ab059819
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2021-01-25 19:33:57 +0100

    Reworked Jackhammer

--- src/main/java/de/hglabor/plugins/kitapi/kit/kits/JackhammerKit.java
+++ src/main/java/de/hglabor/plugins/kitapi/kit/kits/JackhammerKit.java
@@ -7,46 +7,40 @@ import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 
+import java.util.Collections;
 import java.util.Objects;
 
 /**
- * Hommage an Waffel :)
+ * Hommage an Waffel :) (wtf?)
  */
 public class JackhammerKit extends AbstractKit {
+    public final static JackhammerKit INSTANCE = new JackhammerKit();
 
     private JackhammerKit() {
         super("Jackhammer", Material.STONE_AXE);
+        setMainKitItem(getDisplayMaterial(),true);
+        addEvents(Collections.singletonList(BlockBreakEvent.class));
     }
 
     @Override
-    public void onBlockBreak(BlockBreakEvent e) {
+    public void onBlockBreakWithKitItem(BlockBreakEvent e) {
         Block block = e.getBlock();
-        World world = e.getBlock().getWorld();
-        Location loc = block.getLocation();
-        int add = 0;
-
         Material above = block.getRelative(BlockFace.UP).getType();
         Material below = block.getRelative(BlockFace.DOWN).getType();
-
-        if (above == Material.AIR && below == Material.AIR) {
-            return;
-        }
 
         if (above != Material.AIR && below == Material.AIR) {
             //HOCH
             dig(block.getLocation(), 1, 1);
-
         } else if (above == Material.AIR) {
             //RUNTER
             dig(block.getLocation(), -1, 1);
-
         } else {
             //BEIDE RICHTUNGEN
             dig(block.getLocation(), 1, 2);
             dig(block.getLocation(), -1, 2);
         }
-
     }
 
     /**
@@ -58,13 +52,14 @@ public class JackhammerKit extends AbstractKit {
 
         Bukkit.getScheduler().runTaskTimer(KitManager.getInstance().getPlugin(), r -> {
             currentLocation.getBlock().setType(Material.AIR);
-            Objects.requireNonNull(loc.getWorld()).spawnParticle(Particle.ASH, currentLocation.clone().add(.5, 0, .5), 100);
+            loc.getWorld().spawnParticle(Particle.ASH, currentLocation.clone().add(.5, 0, .5), 100);
             currentLocation.add(0, direction, 0);
+            if (direction == 1 && currentLocation.getBlockY() >= currentLocation.getWorld().getMaxHeight()) {
+                r.cancel();
+            }
             if (currentLocation.getBlock().getType() == Material.BEDROCK) {
                 r.cancel();
             }
         }, 0, delay);
-
-
     }
 }
