9d53300483f6441955b7b5220608b23793652c58
mooziii <63669478+mooz-lego@users.noreply.github.com>
2021-01-25 12:24:59 +0100

    Fix ReviveKit

--- src/main/java/de/hglabor/plugins/kitapi/kit/kits/ReviveKit.java
+++ src/main/java/de/hglabor/plugins/kitapi/kit/kits/ReviveKit.java
@@ -1,6 +1,7 @@
 package de.hglabor.plugins.kitapi.kit.kits;
 
 import de.hglabor.plugins.kitapi.kit.AbstractKit;
+import de.hglabor.plugins.kitapi.kit.KitManager;
 import de.hglabor.plugins.kitapi.player.KitPlayer;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -13,19 +14,43 @@ public class ReviveKit extends AbstractKit {
 
     private ReviveKit() {
         super("Revive", Material.TOTEM_OF_UNDYING, 60);
-    }
-
-    @Override
-    public void enable(KitPlayer kitPlayer) {
-        Player player = Bukkit.getPlayer(kitPlayer.getUUID());
-        player.getInventory().setItemInOffHand(new ItemStack(Material.TOTEM_OF_UNDYING));
+        setMainKitItem(getDisplayMaterial());
+        setUsesOffHand(true);
     }
 
     @Override
     public void disable(KitPlayer kitPlayer) {
         Player player = Bukkit.getPlayer(kitPlayer.getUUID());
-        if(player.getInventory().getItemInOffHand().getType() == Material.TOTEM_OF_UNDYING) {
+        if (player.getInventory().getItemInOffHand().isSimilar(this.getMainKitItem())) {
             player.getInventory().setItemInOffHand(new ItemStack(Material.AIR));
+        }
+    }
+
+
+    @Override
+    public void enable(KitPlayer kitPlayer) {
+        Player player = Bukkit.getPlayer(kitPlayer.getUUID());
+        giveTotem(kitPlayer, player);
+    }
+
+    @Override
+    public void onEntityResurrect(EntityResurrectEvent event) {
+
+        Player player = (Player) event.getEntity();
+        KitPlayer kitPlayer = KitManager.getInstance().getPlayer(player);
+
+        kitPlayer.putKitAttribute(this, kitPlayer.getKitAttribute(this) != null ? (Integer) kitPlayer.getKitAttribute(this) + 1 : 1);
+
+        Bukkit.getScheduler().runTaskLater(Bukkit.getPluginManager().getPlugin("HGLaborFFA2.0"), () -> {
+            giveTotem(kitPlayer, player);
+        }, getCooldown() * 20 * (Integer) kitPlayer.getKitAttribute(this));
+
+
+    }
+
+    private void giveTotem(KitPlayer kitPlayer, Player player) {
+        if (kitPlayer.isValid() && kitPlayer.hasKit(this)) {
+            player.getInventory().setItemInOffHand(this.getMainKitItem());
         }
     }
 }
