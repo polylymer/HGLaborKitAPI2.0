875e91227dc0db43a29dbf4dcb40c5e691c038e0
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2021-02-19 23:55:23 +0100

    Replaced local count attribute with kitAttribute

--- src/main/java/de/hglabor/plugins/kitapi/kit/kits/LumberjackKit.java
+++ src/main/java/de/hglabor/plugins/kitapi/kit/kits/LumberjackKit.java
@@ -1,16 +1,19 @@
 package de.hglabor.plugins.kitapi.kit.kits;
 
+import de.hglabor.plugins.kitapi.KitApi;
 import de.hglabor.plugins.kitapi.kit.AbstractKit;
+import de.hglabor.plugins.kitapi.player.KitPlayer;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.event.block.BlockBreakEvent;
 
 import java.util.Collections;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class LumberjackKit extends AbstractKit {
     public final static LumberjackKit INSTANCE = new LumberjackKit();
-    int count = 0;
+
     private LumberjackKit() {
         super("Lumberjack", Material.WOODEN_AXE);
         setMainKitItem(getDisplayMaterial(), true);
@@ -21,20 +24,21 @@ public class LumberjackKit extends AbstractKit {
     public void onBlockBreakWithKitItem(BlockBreakEvent e) {
         String blockTypeName = e.getBlock().getType().name().toLowerCase();
         if (blockTypeName.contains("wood") || blockTypeName.contains("log")) {
-            count = 0;
-            breakSurroundingWood(e.getBlock());
+            KitPlayer player = KitApi.getInstance().getPlayer(e.getPlayer());
+            player.putKitAttribute(this, new AtomicInteger(), AtomicInteger.class);
+            breakSurroundingWood(e.getBlock(), player);
         }
     }
 
-    public void breakSurroundingWood(Block block) {
+    public void breakSurroundingWood(Block block, KitPlayer kitPlayer) {
         String blockTypeName = block.getType().name().toLowerCase();
         if (blockTypeName.contains("wood") || blockTypeName.contains("log")) {
             block.breakNaturally();
-            count++;
-            if (count > 1500) return;
-            BlockFace[] faces = { BlockFace.DOWN, BlockFace.UP, BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST};
+            AtomicInteger count = kitPlayer.getKitAttribute(this, AtomicInteger.class);
+            if (count.getAndIncrement() > 1500) return;
+            BlockFace[] faces = {BlockFace.DOWN, BlockFace.UP, BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST};
             for (BlockFace face : faces) {
-                breakSurroundingWood(block.getRelative(face));
+                breakSurroundingWood(block.getRelative(face), kitPlayer);
             }
         }
     }
