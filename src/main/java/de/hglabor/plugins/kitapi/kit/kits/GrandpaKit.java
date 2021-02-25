d00716bb82877e5e03fd3412f82a6ed0be20e0ff
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2021-02-25 14:34:51 +0100

    Added ItemBuilder for Grandpa

--- src/main/java/de/hglabor/plugins/kitapi/kit/kits/GrandpaKit.java
+++ src/main/java/de/hglabor/plugins/kitapi/kit/kits/GrandpaKit.java
@@ -1,6 +1,7 @@
 package de.hglabor.plugins.kitapi.kit.kits;
 
 import de.hglabor.plugins.kitapi.kit.AbstractKit;
+import de.hglabor.utils.noriskutils.ItemBuilder;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
@@ -9,14 +10,11 @@ import org.bukkit.inventory.ItemStack;
  * @author Hotkeyyy
  * @since 2021/02/25
  */
-
 public class GrandpaKit extends AbstractKit {
     public static final GrandpaKit INSTANCE = new GrandpaKit();
 
-    protected GrandpaKit() {
+    private GrandpaKit() {
         super("Grandpa", Material.STICK);
-        ItemStack kitItem = new ItemStack(Material.STICK);
-        kitItem.addEnchantment(Enchantment.KNOCKBACK,2);
-        setMainKitItem(kitItem);
+        setMainKitItem(new ItemBuilder(Material.STICK).setEnchantment(Enchantment.KNOCKBACK, 2).build());
     }
 }
