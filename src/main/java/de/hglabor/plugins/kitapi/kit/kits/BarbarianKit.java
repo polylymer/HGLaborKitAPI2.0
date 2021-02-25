40a0c49417e5cb2a1ac4b3080a559d1af5f30268
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2021-02-25 17:10:26 +0100

    Added isKitItem and TODOs for Barbarian

--- src/main/java/de/hglabor/plugins/kitapi/kit/kits/BarbarianKit.java
+++ src/main/java/de/hglabor/plugins/kitapi/kit/kits/BarbarianKit.java
@@ -2,6 +2,7 @@ package de.hglabor.plugins.kitapi.kit.kits;
 
 import de.hglabor.plugins.kitapi.kit.AbstractKit;
 import de.hglabor.plugins.kitapi.player.KitPlayer;
+import de.hglabor.utils.noriskutils.ItemBuilder;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
@@ -11,77 +12,82 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 /**
  * @author Hotkeyyy
  * @since 2021/02/25
  */
-
 public class BarbarianKit extends AbstractKit {
     public static final BarbarianKit INSTANCE = new BarbarianKit();
 
-    HashMap<UUID, Integer> playerBarbarianLevel = new HashMap<UUID, Integer>();
+    Map<UUID, Integer> playerBarbarianLevel;
 
     private BarbarianKit() {
-
         super("Barbarian", Material.WOODEN_SWORD);
-
-        ItemStack kitItem = new ItemStack(Material.WOODEN_SWORD);
-        kitItem.addEnchantment(Enchantment.DURABILITY, 3);
-        ItemMeta kitItemMeta = kitItem.getItemMeta();
-        kitItemMeta.setUnbreakable(true);
-        kitItemMeta.setLocalizedName("Barbarian");
-        kitItem.setItemMeta(kitItemMeta);
-
-        setMainKitItem(kitItem);
-
+        setMainKitItem(new ItemBuilder(Material.WOODEN_SWORD).setLocalizedName(this.getName()).setUnbreakable(true).build());
         addEvents(Collections.singletonList(PlayerDeathEvent.class));
     }
 
     @Override
     public void onPlayerKillsPlayer(KitPlayer killer, KitPlayer dead) {
-        if (!killer.hasKit(this)) return;
-        if (!Bukkit.getPlayer(killer.getUUID()).getInventory().getItemInMainHand().getItemMeta().getLocalizedName().equals(getMainKitItem().getItemMeta().getLocalizedName()))
+        Player player = Bukkit.getPlayer(killer.getUUID());
+        UUID killerUUID = killer.getUUID();
+        if (player == null) {
             return;
-        if (!playerBarbarianLevel.containsKey(killer.getUUID())) playerBarbarianLevel.put(killer.getUUID(), 1);
-        else playerBarbarianLevel.put(killer.getUUID(), playerBarbarianLevel.get(killer.getUUID()) + 1);
-        Player p = Bukkit.getPlayer(killer.getUUID());
-        switch (playerBarbarianLevel.get(killer.getUUID())) {
+        }
+        ItemStack barbarianSword = player.getInventory().getItemInMainHand();
+        if (!isKitItem(barbarianSword)) {
+            return;
+        }
+        if (!playerBarbarianLevel.containsKey(killerUUID)) playerBarbarianLevel.put(killerUUID, 1);
+        else playerBarbarianLevel.put(killerUUID, playerBarbarianLevel.get(killerUUID) + 1);
+        //TODO KitAttribute and dynamic values
+        switch (playerBarbarianLevel.get(killerUUID)) {
             case 1:
-                p.getInventory().getItemInMainHand().setType(Material.STONE_SWORD);
+                barbarianSword.setType(Material.STONE_SWORD);
                 break;
             case 2:
-                p.getInventory().getItemInMainHand().setType(Material.WOODEN_SWORD);
-                p.getInventory().getItemInMainHand().addEnchantment(Enchantment.DAMAGE_ALL, 1);
+                barbarianSword.setType(Material.WOODEN_SWORD);
+                barbarianSword.addEnchantment(Enchantment.DAMAGE_ALL, 1);
                 break;
             case 3:
-                p.getInventory().getItemInMainHand().removeEnchantment(Enchantment.DAMAGE_ALL);
-                p.getInventory().getItemInMainHand().setType(Material.IRON_SWORD);
+                barbarianSword.removeEnchantment(Enchantment.DAMAGE_ALL);
+                barbarianSword.setType(Material.IRON_SWORD);
                 break;
             case 4:
-                p.getInventory().getItemInMainHand().setType(Material.STONE_SWORD);
-                p.getInventory().getItemInMainHand().addEnchantment(Enchantment.DAMAGE_ALL, 2);
+                barbarianSword.setType(Material.STONE_SWORD);
+                barbarianSword.addEnchantment(Enchantment.DAMAGE_ALL, 2);
                 break;
             case 5:
-                p.getInventory().getItemInMainHand().addEnchantment(Enchantment.FIRE_ASPECT, 1);
+                barbarianSword.addEnchantment(Enchantment.FIRE_ASPECT, 1);
                 break;
             case 6:
-                p.getInventory().getItemInMainHand().removeEnchantment(Enchantment.FIRE_ASPECT);
-                p.getInventory().getItemInMainHand().removeEnchantment(Enchantment.DAMAGE_ALL);
-                p.getInventory().getItemInMainHand().setType(Material.DIAMOND_SWORD);
+                barbarianSword.removeEnchantment(Enchantment.FIRE_ASPECT);
+                barbarianSword.removeEnchantment(Enchantment.DAMAGE_ALL);
+                barbarianSword.setType(Material.DIAMOND_SWORD);
                 break;
             case 7:
-                p.getInventory().getItemInMainHand().addEnchantment(Enchantment.DAMAGE_ALL, 1);
+                barbarianSword.addEnchantment(Enchantment.DAMAGE_ALL, 1);
                 break;
             case 8:
-                p.getInventory().getItemInMainHand().addEnchantment(Enchantment.DAMAGE_ALL, 2);
+                barbarianSword.addEnchantment(Enchantment.DAMAGE_ALL, 2);
                 break;
             case 9:
-                p.getInventory().getItemInMainHand().addEnchantment(Enchantment.DAMAGE_ALL, 3);
+                barbarianSword.addEnchantment(Enchantment.DAMAGE_ALL, 3);
                 break;
         }
+    }
 
+    @Override
+    public boolean isKitItem(ItemStack itemStack) {
+        if (itemStack.hasItemMeta()) {
+            ItemMeta itemMeta = itemStack.getItemMeta();
+            if (itemMeta.hasLocalizedName()) {
+                return itemMeta.getLocalizedName().equalsIgnoreCase(this.getName());
+            }
+        }
+        return false;
     }
 }
