fe4416227c5b1c0e503da495172ee227f8073ad8
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2021-03-07 16:13:35 +0100

    Adjust Turtle to Annotation System

--- src/main/java/de/hglabor/plugins/kitapi/kit/kits/TurtleKit.java
+++ src/main/java/de/hglabor/plugins/kitapi/kit/kits/TurtleKit.java
@@ -1,13 +1,14 @@
 package de.hglabor.plugins.kitapi.kit.kits;
 
 import de.hglabor.plugins.kitapi.kit.AbstractKit;
+import de.hglabor.plugins.kitapi.kit.events.KitEvent;
+import de.hglabor.plugins.kitapi.kit.settings.DoubleArg;
 import de.hglabor.plugins.kitapi.player.KitPlayer;
 import org.bukkit.Material;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-
-import java.util.Collections;
+import org.bukkit.event.entity.EntityDamageEvent;
 
 /**
  * @author Hotkeyyy
@@ -17,22 +18,30 @@ import java.util.Collections;
 public class TurtleKit extends AbstractKit {
     public static final TurtleKit INSTANCE = new TurtleKit();
 
+    @DoubleArg(min = 0.1)
+    private final double dealingDamage, damageTrigger, damageReplacement;
+
     private TurtleKit() {
         super("Turtle", Material.TURTLE_HELMET);
-        addEvents(Collections.singletonList(EntityDamageByEntityEvent.class));
+        dealingDamage = 0.5D;
+        damageTrigger = 0.5D;
+        damageReplacement = 0.5D;
     }
 
+    @KitEvent
     @Override
-    public void onPlayerGetsAttackedByLivingEntity(EntityDamageByEntityEvent event, Player player, LivingEntity attacker) {
-        if (event.getFinalDamage() > 0.5 && player.isSneaking()) {
-            event.setDamage(0.5);
+    public void onEntityDamage(EntityDamageEvent event) {
+        Player player = (Player) event.getEntity();
+        if (event.getDamage() > damageTrigger && player.isSneaking()) {
+            event.setDamage(damageReplacement);
         }
     }
 
+    @KitEvent
     @Override
     public void onPlayerAttacksLivingEntity(EntityDamageByEntityEvent event, KitPlayer attacker, LivingEntity entity) {
         attacker.getBukkitPlayer().ifPresent(player -> {
-            if (player.isSneaking()) event.setDamage(0.5);
+            if (player.isSneaking()) event.setDamage(dealingDamage);
         });
     }
 }
