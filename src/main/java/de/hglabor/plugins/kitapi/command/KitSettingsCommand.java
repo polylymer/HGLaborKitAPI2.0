e7fe33e70bb9ed2f042ca7a8542e60a603ec7f35
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2021-03-07 12:20:17 +0100

    Show current Value, added Permissions, notify players

--- src/main/java/de/hglabor/plugins/kitapi/command/KitSettingsCommand.java
+++ src/main/java/de/hglabor/plugins/kitapi/command/KitSettingsCommand.java
@@ -3,52 +3,62 @@ package de.hglabor.plugins.kitapi.command;
 import de.hglabor.plugins.kitapi.KitApi;
 import de.hglabor.plugins.kitapi.kit.AbstractKit;
 import de.hglabor.plugins.kitapi.kit.settings.*;
+import de.hglabor.utils.noriskutils.PermissionUtils;
 import de.hglabor.utils.noriskutils.ReflectionUtils;
 import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.arguments.Argument;
 import dev.jorel.commandapi.arguments.CustomArgument;
-import dev.jorel.commandapi.arguments.DoubleArgument;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.util.NumberConversions;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
-import java.sql.Ref;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 public class KitSettingsCommand {
-    public KitSettingsCommand() {
+    private static final String PERMISSION = "hglabor.kitapi.changeKitSettings";
+
+    public KitSettingsCommand(boolean checkForHigherRank) {
         new CommandAPICommand("kitsettings")
                 .withArguments(kitArgument())
                 .withArguments(kitSettings())
                 .withArguments(settingValues())
+                .withPermission(PERMISSION)
                 .executesPlayer((player, objects) -> {
+                    if (checkForHigherRank) {
+                        if (PermissionUtils.checkForHigherRank(player)) {
+                            player.sendMessage(ChatColor.RED + "Player with higher rank is online.");
+                            return;
+                        }
+                    }
                     AbstractKit kit = (AbstractKit) objects[0];
                     String fieldName = (String) objects[1];
                     String value = (String) objects[2];
                     String replacement = "";
                     Field field = ReflectionUtils.getField(kit.getClass(), fieldName);
                     if (ReflectionUtils.isFloat(field)) {
-                        FloatArg annotation = getAnnotation(field.getDeclaredAnnotations(), kit.getClass());
+                        FloatArg annotation = getAnnotation(field.getDeclaredAnnotations(), FloatArg.class);
                         float max = Math.max(annotation.min(), Math.min(annotation.max(), NumberConversions.toFloat(value)));
                         replacement = String.valueOf(max);
                         ReflectionUtils.set(field, kit, max);
                     } else if (ReflectionUtils.isDouble(field)) {
-                        DoubleArg annotation = getAnnotation(field.getDeclaredAnnotations(), kit.getClass());
+                        DoubleArg annotation = getAnnotation(field.getDeclaredAnnotations(), DoubleArg.class);
                         double max = Math.max(annotation.min(), Math.min(annotation.max(), NumberConversions.toDouble(value)));
                         replacement = String.valueOf(max);
                         ReflectionUtils.set(field, kit, max);
                     } else if (ReflectionUtils.isInt(field)) {
-                        IntArg annotation = getAnnotation(field.getDeclaredAnnotations(), kit.getClass());
+                        IntArg annotation = getAnnotation(field.getDeclaredAnnotations(), IntArg.class);
                         int max = Math.max(annotation.min(), Math.min(annotation.max(), NumberConversions.toInt(value)));
                         replacement = String.valueOf(max);
                         ReflectionUtils.set(field, kit, max);
                     } else if (ReflectionUtils.isLong(field)) {
-                        LongArg annotation = getAnnotation(field.getDeclaredAnnotations(), kit.getClass());
+                        LongArg annotation = getAnnotation(field.getDeclaredAnnotations(), LongArg.class);
                         long max = Math.max(annotation.min(), Math.min(annotation.max(), NumberConversions.toLong(value)));
                         replacement = String.valueOf(max);
                         ReflectionUtils.set(field, kit, max);
@@ -63,7 +73,16 @@ public class KitSettingsCommand {
     }
 
     private void sendMessage(Player player, AbstractKit kit, String fieldName, String value) {
-        player.sendMessage(kit.getName() + " -> " + fieldName + " -> " + value);
+        String message = ChatColor.AQUA.toString() + ChatColor.BOLD + kit.getName() +
+                ChatColor.RESET + " -> " + fieldName +
+                " -> " + ChatColor.GOLD + value;
+        for (Player otherPlayer : Bukkit.getOnlinePlayers()) {
+            if (otherPlayer == player) continue;
+            if (otherPlayer.hasPermission(PERMISSION)) {
+                otherPlayer.sendMessage(otherPlayer.getName() + " changed " + message);
+            }
+        }
+        player.sendMessage(message);
     }
 
     private Argument kitArgument() {
@@ -80,7 +99,8 @@ public class KitSettingsCommand {
     private Argument kitSettings() {
         return new CustomArgument<>("settings", (input) -> input).overrideSuggestions((commandSender, objects) -> {
             AbstractKit kit = (AbstractKit) objects[0];
-            return getAnnotatedFieldNames(kit).toArray(String[]::new);
+            String[] strings = getAnnotatedFieldNames(kit).toArray(String[]::new);
+            return strings.length == 0 ? new String[]{"NO_SETTINGS"} : strings;
         });
     }
 
@@ -89,27 +109,37 @@ public class KitSettingsCommand {
             AbstractKit kit = (AbstractKit) objects[0];
             String fieldName = (String) objects[1];
             Field field = ReflectionUtils.getField(kit.getClass(), fieldName);
+            if (field == null) return new String[]{"null"};
+            String currentValue = "Current Value: " + ReflectionUtils.get(field, kit).toString();
             if (ReflectionUtils.isFloat(field)) {
-                FloatArg annotation = getAnnotation(field.getDeclaredAnnotations(), kit.getClass());
-                return new String[]{"Float value from " + annotation.min() + " to " + annotation.max()};
+                FloatArg annotation = getAnnotation(field.getDeclaredAnnotations(), FloatArg.class);
+                if (annotation != null) {
+                    return new String[]{currentValue, "Float value from " + annotation.min() + " to " + annotation.max()};
+                }
             } else if (ReflectionUtils.isDouble(field)) {
-                DoubleArg annotation = getAnnotation(field.getDeclaredAnnotations(), kit.getClass());
-                return new String[]{"Double value from " + annotation.min() + " to " + annotation.max()};
+                DoubleArg annotation = getAnnotation(field.getDeclaredAnnotations(), DoubleArg.class);
+                if (annotation != null)
+                    return new String[]{currentValue,"Double value from " + annotation.min() + " to " + annotation.max()};
             } else if (ReflectionUtils.isInt(field)) {
-                IntArg annotation = getAnnotation(field.getDeclaredAnnotations(), kit.getClass());
-                return new String[]{"Double value from " + annotation.min() + " to " + annotation.max()};
+                IntArg annotation = getAnnotation(field.getDeclaredAnnotations(), IntArg.class);
+                if (annotation != null)
+                    return new String[]{currentValue,"Double value from " + annotation.min() + " to " + annotation.max()};
             } else if (ReflectionUtils.isLong(field)) {
-                LongArg annotation = getAnnotation(field.getDeclaredAnnotations(), kit.getClass());
-                return new String[]{"Long value from " + annotation.min() + " to " + annotation.max()};
+                LongArg annotation = getAnnotation(field.getDeclaredAnnotations(), LongArg.class);
+                if (annotation != null)
+                    return new String[]{currentValue,"Long value from " + annotation.min() + " to " + annotation.max()};
             } else if (ReflectionUtils.isMaterial(field)) {
-                return Arrays.stream(Material.values()).map(Material::name).toArray(String[]::new);
+                List<String> list = new ArrayList<>();
+                list.add(currentValue);
+                Arrays.stream(Material.values()).map(Enum::name).forEach(list::add);
+                return list.toArray(new String[0]);
             }
             return new String[]{"error haha"};
         });
     }
 
     @SuppressWarnings("unchecked")
-    private <T extends Annotation> T getAnnotation(Annotation[] annotations, Class<? extends AbstractKit> clazz) {
+    private <T extends Annotation> T getAnnotation(Annotation[] annotations, Class<T> clazz) {
         for (Annotation annotation : annotations) {
             if (annotation.annotationType() == clazz) {
                 return (T) annotation;
@@ -124,8 +154,9 @@ public class KitSettingsCommand {
                 LongArg.class, MaterialArg.class, StringArg.class);
         List<String> names = new ArrayList<>();
         for (Field field : kit.getClass().getDeclaredFields()) {
-            for (Annotation annotation : field.getAnnotations()) {
-                if (kitAnnotations.contains(annotation.getClass())) {
+            for (Annotation annotation : field.getDeclaredAnnotations()) {
+                System.out.println(annotation);
+                if (kitAnnotations.contains(annotation.annotationType())) {
                     names.add(field.getName());
                 }
             }
