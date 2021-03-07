43292497982f56d47a5a9ac3ef7f43e3063f93a0
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2021-03-07 08:26:59 +0100

    Refactored Barbarian with new Annotation System

--- src/main/java/de/hglabor/plugins/kitapi/kit/kits/BarbarianKit.java
+++ src/main/java/de/hglabor/plugins/kitapi/kit/kits/BarbarianKit.java
@@ -14,9 +14,6 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
 /**
@@ -27,13 +24,14 @@ public class BarbarianKit extends AbstractKit {
     public static final BarbarianKit INSTANCE = new BarbarianKit();
     @IntArg
     private final int moduloSteps;
-
-    private final Map<UUID, Integer> playerBarbarianLevel;
+    private final String killsKey;
+    private final String levelKey;
 
     private BarbarianKit() {
         super("Barbarian", Material.WOODEN_SWORD);
         moduloSteps = 1;
-        playerBarbarianLevel = new HashMap<>();
+        killsKey = this.getName() + "kills";
+        levelKey = this.getName() + "level";
         setMainKitItem(new ItemBuilder(Material.WOODEN_SWORD).setLocalizedName(this.getName()).setUnbreakable(true).build());
     }
 
@@ -41,7 +39,6 @@ public class BarbarianKit extends AbstractKit {
     @Override
     public void onPlayerKillsPlayer(KitPlayer killer, KitPlayer dead) {
         Player player = Bukkit.getPlayer(killer.getUUID());
-        UUID killerUUID = killer.getUUID();
         if (player == null) {
             return;
         }
@@ -50,44 +47,43 @@ public class BarbarianKit extends AbstractKit {
             return;
         }
         KitPlayer kitPlayer = KitApi.getInstance().getPlayer(player);
-        //TODO MIT Keys replacen sheeeesh
-        // kitPlayer.putKitAttribute();
-        if (!playerBarbarianLevel.containsKey(killerUUID)) playerBarbarianLevel.put(killerUUID, 1);
-        else playerBarbarianLevel.put(killerUUID, playerBarbarianLevel.get(killerUUID) + 1);
-        //TODO KitAttribute and dynamic values
-        switch (playerBarbarianLevel.get(killerUUID)) {
-            case 1:
-                barbarianSword.setType(Material.STONE_SWORD);
-                break;
-            case 2:
-                barbarianSword.setType(Material.WOODEN_SWORD);
-                barbarianSword.addEnchantment(Enchantment.DAMAGE_ALL, 1);
-                break;
-            case 3:
-                barbarianSword.removeEnchantment(Enchantment.DAMAGE_ALL);
-                barbarianSword.setType(Material.IRON_SWORD);
-                break;
-            case 4:
-                barbarianSword.setType(Material.STONE_SWORD);
-                barbarianSword.addEnchantment(Enchantment.DAMAGE_ALL, 2);
-                break;
-            case 5:
-                barbarianSword.addEnchantment(Enchantment.FIRE_ASPECT, 1);
-                break;
-            case 6:
-                barbarianSword.removeEnchantment(Enchantment.FIRE_ASPECT);
-                barbarianSword.removeEnchantment(Enchantment.DAMAGE_ALL);
-                barbarianSword.setType(Material.DIAMOND_SWORD);
-                break;
-            case 7:
-                barbarianSword.addEnchantment(Enchantment.DAMAGE_ALL, 1);
-                break;
-            case 8:
-                barbarianSword.addEnchantment(Enchantment.DAMAGE_ALL, 2);
-                break;
-            case 9:
-                barbarianSword.addEnchantment(Enchantment.DAMAGE_ALL, 3);
-                break;
+        kitPlayer.putKitAttribute(killsKey, kitPlayer.getKitAttributeOrDefault(killsKey, 0) + 1);
+        if ((int) kitPlayer.getKitAttribute(killsKey) % moduloSteps == 0) {
+            switch (kitPlayer.getKitAttributeOrDefault(levelKey, 1)) {
+                case 1:
+                    barbarianSword.setType(Material.STONE_SWORD);
+                    break;
+                case 2:
+                    barbarianSword.setType(Material.WOODEN_SWORD);
+                    barbarianSword.addEnchantment(Enchantment.DAMAGE_ALL, 1);
+                    break;
+                case 3:
+                    barbarianSword.removeEnchantment(Enchantment.DAMAGE_ALL);
+                    barbarianSword.setType(Material.IRON_SWORD);
+                    break;
+                case 4:
+                    barbarianSword.setType(Material.STONE_SWORD);
+                    barbarianSword.addEnchantment(Enchantment.DAMAGE_ALL, 2);
+                    break;
+                case 5:
+                    barbarianSword.addEnchantment(Enchantment.FIRE_ASPECT, 1);
+                    break;
+                case 6:
+                    barbarianSword.removeEnchantment(Enchantment.FIRE_ASPECT);
+                    barbarianSword.removeEnchantment(Enchantment.DAMAGE_ALL);
+                    barbarianSword.setType(Material.DIAMOND_SWORD);
+                    break;
+                case 7:
+                    barbarianSword.addEnchantment(Enchantment.DAMAGE_ALL, 1);
+                    break;
+                case 8:
+                    barbarianSword.addEnchantment(Enchantment.DAMAGE_ALL, 2);
+                    break;
+                case 9:
+                    barbarianSword.addEnchantment(Enchantment.DAMAGE_ALL, 3);
+                    break;
+            }
+            kitPlayer.putKitAttribute(levelKey, kitPlayer.getKitAttributeOrDefault(levelKey, 1));
         }
     }
 
