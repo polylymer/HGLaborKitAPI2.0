5ead983e7d85a00d52d2366f3a99c791b3a1e7fe
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2021-03-31 18:24:16 +0200

    fixed CPS-Counter nicht korrekt #73

--- src/main/java/de/hglabor/plugins/kitapi/pvp/CPSChecker.java
+++ src/main/java/de/hglabor/plugins/kitapi/pvp/CPSChecker.java
@@ -1,9 +1,12 @@
 package de.hglabor.plugins.kitapi.pvp;
 
 import de.hglabor.plugins.kitapi.KitApi;
+import de.hglabor.plugins.kitapi.config.KitApiConfig;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class CPSChecker implements Listener {
@@ -13,5 +16,13 @@ public class CPSChecker implements Listener {
         if (event.getAction().equals(Action.LEFT_CLICK_AIR) || event.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
             KitApi.getInstance().getPlayer(event.getPlayer()).addLeftClick(System.currentTimeMillis());
         }
+    }
+
+    @EventHandler
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+        if (!(event.getDamager() instanceof Player)) {
+            return;
+        }
+        KitApi.getInstance().getPlayer((Player) event.getDamager()).addLeftClick(System.currentTimeMillis());
     }
 }
