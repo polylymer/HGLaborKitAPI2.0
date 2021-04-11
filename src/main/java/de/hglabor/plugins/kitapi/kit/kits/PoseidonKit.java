6054bd6a116be05445ad9e55ce119abc7fa69b7d
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2021-04-11 16:42:45 +0200

    Added PoseidonRain Runnable +  changed rain to packets

--- src/main/java/de/hglabor/plugins/kitapi/kit/kits/PoseidonKit.java
+++ src/main/java/de/hglabor/plugins/kitapi/kit/kits/PoseidonKit.java
@@ -5,93 +5,116 @@ import de.hglabor.plugins.kitapi.kit.AbstractKit;
 import de.hglabor.plugins.kitapi.kit.events.KitEvent;
 import de.hglabor.plugins.kitapi.kit.settings.IntArg;
 import de.hglabor.plugins.kitapi.player.KitPlayer;
+import de.hglabor.utils.noriskutils.ChatUtils;
 import de.hglabor.utils.noriskutils.ItemBuilder;
+import de.hglabor.utils.noriskutils.NMSUtils;
+import net.minecraft.server.v1_16_R3.PacketPlayOutGameStateChange;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.boss.BossBar;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.weather.WeatherChangeEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 
-public class PoseidonKit extends AbstractKit {
+import static de.hglabor.utils.localization.Localization.t;
 
+public class PoseidonKit extends AbstractKit implements Listener {
     public static final PoseidonKit INSTANCE = new PoseidonKit();
 
-    @IntArg
+    @IntArg(min = 0)
     private final int rainTime;
-    private final String isInWaterKey;
+    @IntArg
+    private final int speedAmplifier;
+    @IntArg
+    private final int regenerationAmplifier;
+    private final String rainRunnable;
 
     private PoseidonKit() {
-        super("Poseidon", Material.TRIDENT);
-        setMainKitItem(new ItemBuilder(getMainKitItem()).setEnchantment(Enchantment.LOYALTY, 3).setUnbreakable(true).build());
-        rainTime = 10;
-        isInWaterKey = this.getName() + "isInWater";
+        super("Poseidon", new ItemBuilder(Material.TRIDENT).setEnchantment(Enchantment.RIPTIDE, 3).setName("Poseidon").build());
+        rainTime = 25;
+        speedAmplifier = 0;
+        regenerationAmplifier = 0;
+        rainRunnable = this.getName() + "rainRunnable";
     }
 
     @Override
-    public void onDeactivation(KitPlayer kitPlayer) {
-        // sollte durch https://github.com/HGLabor/HGLaborKitAPI2.0/commit/1b37b91c9827d4098446392930b710e94b847a0e?branch=1b37b91c9827d4098446392930b710e94b847a0e&diff=unified nicht mehr useable sein glaube dann auch wenn nicht dann ist blöd
-        ItemMeta kititemMeta = getMainKitItem().getItemMeta();
-        clearEnchants(kititemMeta);
-        getMainKitItem().setItemMeta(kititemMeta);
+    public void onDisable(KitPlayer kitPlayer) {
+        if (kitPlayer.getKitAttribute(rainRunnable) != null) {
+            ((PoseidonRain) kitPlayer.getKitAttribute(rainRunnable)).stop();
+        }
     }
 
-    @KitEvent
-    public void onPlayerMoveEvent(PlayerMoveEvent event, KitPlayer kitPlayer) {
+    @EventHandler
+    public void onPlayerToggleSneak(PlayerToggleSneakEvent event) {
         Player player = event.getPlayer();
-
-        if (kitPlayer.getKitAttributeOrDefault(isInWaterKey, false)) setRainEnchantments(getMainKitItem()); // nicht sicher ob das besonders effizient ist bitte besser machen wenn geht
-        else setNormalEnchantments(getMainKitItem());
-
-        if (player.getWorld().hasStorm()) {
-            kitPlayer.putKitAttribute(isInWaterKey, true);
-            return;
+        KitPlayer killer = KitApi.getInstance().getPlayer(player);
+        if (killer.getKitAttribute(rainRunnable) != null) {
+            ((PoseidonRain) killer.getKitAttribute(rainRunnable)).addTime(rainTime * 1000L);
+        } else {
+            PoseidonRain poseidonRain = new PoseidonRain(player);
+            killer.putKitAttribute(rainRunnable, poseidonRain);
+            poseidonRain.runTaskTimer(KitApi.getInstance().getPlugin(), 0, 20);
         }
-
-        Material eyeLocationBlock = player.getEyeLocation().getBlock().getType();
-        Material locationBlock = player.getLocation().getBlock().getType();
-
-        if (eyeLocationBlock == Material.WATER || locationBlock == Material.WATER) kitPlayer.putKitAttribute(isInWaterKey, true);
-        else kitPlayer.putKitAttribute(isInWaterKey, false);
     }
 
     @KitEvent
     public void onPlayerKillsPlayer(KitPlayer killer, KitPlayer victim) {
-        if (killer.getBukkitPlayer().isEmpty()) return;
-        Player player = killer.getBukkitPlayer().get();
-        boolean weatherClearBefore = player.getWorld().isClearWeather();
-        player.getWorld().setStorm(true);
-        Bukkit.getScheduler().scheduleSyncDelayedTask(KitApi.getInstance().getPlugin(), () -> player.getWorld().setStorm(!weatherClearBefore), rainTime * 20L);
-    }
-
-
-    private void setRainEnchantments(ItemStack trident) {
-        if (trident.getType() != Material.TRIDENT) return;
-        ItemMeta tridentMeta = trident.getItemMeta();
-        if (tridentMeta == null) return;
-        clearEnchants(tridentMeta);
-        tridentMeta.addEnchant(Enchantment.RIPTIDE, 3, true);
-        trident.setItemMeta(tridentMeta);
-    }
-
-    private void setNormalEnchantments(ItemStack trident) {
-        if (trident.getType() != Material.TRIDENT) return;
-        ItemMeta tridentMeta = trident.getItemMeta();
-        if (tridentMeta == null) return;
-        clearEnchants(tridentMeta);
-        tridentMeta.addEnchant(Enchantment.LOYALTY, 3, true);
-        trident.setItemMeta(tridentMeta);
-    }
-
-    private void clearEnchants(ItemMeta itemMeta) {
-        if (itemMeta == null) return;
-        if (itemMeta.hasEnchants()) {
-            for (Enchantment enchantment : itemMeta.getEnchants().keySet()) {
-                itemMeta.removeEnchant(enchantment);
+        killer.getBukkitPlayer().ifPresent(player -> {
+            if (killer.getKitAttribute(rainRunnable) != null) {
+                ((PoseidonRain) killer.getKitAttribute(rainRunnable)).addTime(rainTime * 1000L);
+            } else {
+                PoseidonRain poseidonRain = new PoseidonRain(player);
+                killer.putKitAttribute(rainRunnable, poseidonRain);
+                poseidonRain.runTaskTimer(KitApi.getInstance().getPlugin(), 0, 20);
             }
+        });
+    }
+
+    private final class PoseidonRain extends BukkitRunnable {
+        private final Player player;
+        private final BossBar rainBar;
+        private long endTime;
+
+        private PoseidonRain(Player player) {
+            this.rainBar = Bukkit.createBossBar(t("poseidon.rain", ChatUtils.getPlayerLocale(player)), BarColor.BLUE, BarStyle.SOLID);
+            this.player = player;
+            this.startRain();
+        }
+
+        private void startRain() {
+            // PacketPlayOutGameStateChange.c = End rain
+            PacketPlayOutGameStateChange rainPacket = new PacketPlayOutGameStateChange(PacketPlayOutGameStateChange.c, 0);
+            NMSUtils.sendPacket(player, rainPacket);
+            rainBar.addPlayer(player);
+        }
+
+        @Override
+        public void run() {
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 20, speedAmplifier));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 20, regenerationAmplifier));
+            rainBar.setProgress((double) System.currentTimeMillis() / endTime);
+            if (System.currentTimeMillis() > endTime) {
+                stop();
+            }
+        }
+
+        public void stop() {
+            cancel();
+            // PacketPlayOutGameStateChange.b = End rain
+            PacketPlayOutGameStateChange rainPacket = new PacketPlayOutGameStateChange(PacketPlayOutGameStateChange.b, 0);
+            NMSUtils.sendPacket(player, rainPacket);
+            rainBar.removeAll();
+        }
+
+        public void addTime(long time) {
+            endTime += time;
         }
     }
 }
