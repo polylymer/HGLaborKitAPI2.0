600d75a9fd7d6e1e8f6c1988130eab7d501a9409
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2021-04-12 21:29:52 +0200

    Registered GardenerKit

--- src/main/java/de/hglabor/plugins/kitapi/KitApi.java
+++ src/main/java/de/hglabor/plugins/kitapi/KitApi.java
@@ -157,7 +157,7 @@ public final class KitApi {
         kits.add(AnalystKit.INSTANCE);
         kits.add(KangarooKit.INSTANCE);
         kits.add(HulkKit.INSTANCE);
-        //kits.add(GardenerKit.INSTANCE);
+        kits.add(GardenerKit.INSTANCE);
         kits.add(BeequeenKit.INSTANCE);
         kits.add(PoseidonKit.INSTANCE);
         kits.add(PhantomKit.INSTANCE);
