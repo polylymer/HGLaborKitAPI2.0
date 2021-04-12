20a2884fe121665530109389f7bb89c17075854c
Wxffel <66750759+Wxffel@users.noreply.github.com>
2021-04-12 14:26:39 +0200

    bit more explosionBarrel work

--- src/main/java/de/hglabor/plugins/kitapi/kit/kits/PiratKit.java
+++ src/main/java/de/hglabor/plugins/kitapi/kit/kits/PiratKit.java
@@ -1,12 +1,18 @@
 package de.hglabor.plugins.kitapi.kit.kits;
 
+import com.google.common.collect.ImmutableMap;
 import de.hglabor.plugins.kitapi.KitApi;
 import de.hglabor.plugins.kitapi.kit.MultipleKitItemsKit;
 import de.hglabor.plugins.kitapi.kit.config.KitMetaData;
 import de.hglabor.plugins.kitapi.kit.items.KitItemAction;
 import de.hglabor.plugins.kitapi.kit.settings.FloatArg;
 import de.hglabor.plugins.kitapi.kit.settings.IntArg;
+import de.hglabor.plugins.kitapi.util.Logger;
+import de.hglabor.utils.localization.Localization;
+import de.hglabor.utils.noriskutils.ChatUtils;
 import de.hglabor.utils.noriskutils.ItemBuilder;
+import org.bukkit.*;
+import org.bukkit.block.Barrel;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.World;
@@ -15,11 +21,16 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPistonExtendEvent;
+import org.bukkit.event.block.BlockPistonRetractEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.persistence.PersistentDataType;
 
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -29,92 +40,158 @@ public class PiratKit extends MultipleKitItemsKit {
     public static final PiratKit INSTANCE = new PiratKit();
 
     @IntArg
-    private final int maxExplosionBarrels;
+    private final int explosionBarrelsLimit;
     @FloatArg(min = 0.1F, max = 100f)
     private final float explosionPower;
 
-    private final HashMap<UUID, ArrayList<Block>> explosionBarrels = new HashMap<>();
+    // the max explosionPower (100f!) can't be outrun by this setting
+    @FloatArg(min = 0.1F, max = 100f)
+    private final float maxAdditionalExplosionPower, additionalExplosionPowerStep;
+
+    private final HashMap<UUID, ArrayList<Location>> explosionBarrelsHolder = new HashMap<>();
 
     // KitItems
 
     private final ItemStack canon = new ItemBuilder(Material.FIRE_CHARGE).setName("Kanone").setDescription("Abschuss!!").build();
 
-    // auch mit buttons/druckplatten/hebeln auslösbar
-    private final ItemStack explosionBarrel = new ItemBuilder(Material.BARREL).setName("Pulverfass").setDescription("Explosion??").build();
-
     // rechtsklick = all, linksklick = one by one
     private final ItemStack remoteDetonator = new ItemBuilder(Material.TRIPWIRE_HOOK).setName("Fernzünder").setDescription("Explosion!!").build();
 
+    // auch mit buttons/druckplatten/hebeln auslösbar
+    private final ItemStack explosionBarrel = new ItemBuilder(Material.BARREL).setName("Pulverfass").setDescription("Explosion??").build();
+
+    private final NamespacedKey canonKey = markerKeyKitPirat("canon");
+    private final NamespacedKey remoteDetonatorKey = markerKeyKitPirat("remote_detonator");
+    private final NamespacedKey explosionBarrelKey = markerKeyKitPirat("explosion_barrel");
+
     protected PiratKit() {
         super("Pirat", Material.FIRE_CHARGE);
+        explosionBarrelsLimit = 3;
         Map<KitItemAction, Float> kitActions = Map.of(
                 new KitItemAction(canon, "pirate.canon"), 5F,
                 new KitItemAction(remoteDetonator, "pirate.remoteDetonator"), 5F);
         setItemsAndCooldown(kitActions);
-        maxExplosionBarrels = 3;
         explosionPower = 5f;
+        maxAdditionalExplosionPower = 5f;
+        additionalExplosionPowerStep = 0.5f;
     }
 
-    // fässer mit gunpowder füllen = explosion größer?
     // sollte der fernzünder droppen können? -> müsste dann das item als key nehmen
-    // fass mit piston verschieben = boom
 
     @Override
     public void onPlayerRightClickKitItem(PlayerInteractEvent event) {
-
-        // soll einmalig beim bekommen der items ausgeführt werden, muss noch machen
-        canon.getItemMeta().getPersistentDataContainer().set(markerKeyKitPirat("fire_charge"), PersistentDataType.BYTE, (byte) 1);
-        explosionBarrel.getItemMeta().getPersistentDataContainer().set(markerKeyKitPirat("explosion_barrel"), PersistentDataType.BYTE, (byte) 1);
-        remoteDetonator.getItemMeta().getPersistentDataContainer().set(markerKeyKitPirat("remote_detonator"), PersistentDataType.BYTE, (byte) 1);
 
         Player player = event.getPlayer();
         UUID uuid = player.getUniqueId();
         World world = player.getWorld();
 
-        ArrayList<Block> explosionBarrelsForPlayer = explosionBarrels.get(uuid);
-
-        if (!event.hasItem()) return;
-
-        // Detonator
-        if (event.getItem().isSimilar(remoteDetonator)) { // Wird nicht gleich sein, da es noch KitItem tags bekommt - muss mir was ausdenken, außerdem löst das hier sowieso nur aus, wenn ein KitItem angeklickt wird - gilt das auch für "additionalKitItems"?
-            // Rechtsklick
-            if (isRightclick(event.getAction())) {
-                // Alle Fässer explodieren lassen
-                for (Block b : explosionBarrelsForPlayer) {
-                    if (isExplosionBarrel(b)) // Überprüfen, ob es ein Valides Pulverfass ist
-                        world.createExplosion(b.getLocation(), explosionPower);
+        if (event.hasItem()) {
+            // Detonator
+            if (event.getItem() != null && isRemoteDetonator(event.getItem())) {
+                event.setCancelled(true);
+                if (isRightclick(event.getAction())) {
+                    player.sendMessage(Localization.INSTANCE.getMessage("pirat.detonatedall",
+                            ImmutableMap.of("amount", String.valueOf(explosionBarrelsHolder.get(uuid).size())),
+                            ChatUtils.getPlayerLocale(player)));
+                    // Alle Fässer explodieren lassen
+                    explosionBarrelsHolder.get(uuid).forEach(it -> detonateExplosionBarrel(world, uuid));
+                } else if (isLeftclick(event.getAction())) {
+                    player.sendMessage(Localization.INSTANCE.getMessage("pirat.detonatedone",
+                            ImmutableMap.of("location", String.valueOf(explosionBarrelsHolder.get(uuid).get(0).toString())),
+                            ChatUtils.getPlayerLocale(player)));
+                    // Fass an erster Position explodieren lassen
+                    detonateExplosionBarrel(world, uuid);
                 }
-                // Liste der Fässer säubern - alle Fässer sind schließlich explodiert
-                explosionBarrels.get(uuid).clear();
-            } else if (isLeftclick(event.getAction())) {
-                // Fass an erster Position explodieren lassen
-                if (isExplosionBarrel(explosionBarrelsForPlayer.get(0)))// Überprüfen, ob es ein Valides Pulverfass ist
-                    world.createExplosion(explosionBarrelsForPlayer.get(0).getLocation(), explosionPower);
-                // Das explodierte Fass entfernen
-                explosionBarrelsForPlayer.remove(0);
+            } else if (event.getClickedBlock() != null) {
+                Block clickedBlock = event.getClickedBlock();
+                if (explosionBarrelsHolder.get(uuid).size() < explosionBarrelsLimit) {
+                    // Beim platzieren eines Pulverfasses das Pulverfass als solches markieren und in die Liste eintragen
+                    clickedBlock.setMetadata(KitMetaData.EXPLOSION_BARREL.getKey(), new FixedMetadataValue(KitApi.getInstance().getPlugin(), uuid));
+                    explosionBarrelsHolder.putIfAbsent(uuid, new ArrayList<>());
+                    explosionBarrelsHolder.get(uuid).add(clickedBlock.getLocation());
+                } else {
+                    player.sendMessage(Localization.INSTANCE.getMessage("pirat.explosionbarrelslimitreached", ChatUtils.getPlayerLocale(player)));
+                    event.setCancelled(true);
+                }
             }
         }
-
-        // TODO = Beim platzieren eines Pulverfasses das Pulverfass als solches markieren und in die Liste eintragen
-        Block block = event.getClickedBlock();
-        block.setMetadata(KitMetaData.EXPLOSION_BARREL.getKey(), new FixedMetadataValue(KitApi.getInstance().getPlugin(), ""));
-
     }
 
+    private void detonateExplosionBarrel(World world, UUID playerUUID) {
+        Block explosionBarrel = world.getBlockAt(explosionBarrelsHolder.get(playerUUID).get(0));
+        if (isExplosionBarrel(explosionBarrel)) {
+            explosionBarrel.getWorld().createExplosion(explosionBarrel.getLocation(), Math.min(Math.min(explosionPower + getAdditionalExplosionPower(explosionBarrel), maxAdditionalExplosionPower), 100f));
+            explosionBarrel.setType(Material.AIR);
+            explosionBarrelsHolder.get(playerUUID).remove(0);
+        }
+    }
+
+    private float getAdditionalExplosionPower(Block block) {
+        AtomicInteger amount = new AtomicInteger();
+        Arrays.stream(((Barrel) block).getInventory().getStorageContents()).filter(itemStack -> itemStack.getType() == Material.GUNPOWDER).collect(Collectors.toList()).forEach(itemStack -> amount.addAndGet(itemStack.getAmount()));
+        return amount.get() * additionalExplosionPowerStep;
+    }
 
     // Überprüfen, ob es ein Pulverfass ist und dieses ggf. aus der Liste entfernen
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
-        UUID uuid = event.getPlayer().getUniqueId();
         Block block = event.getBlock();
         if (isExplosionBarrel(block)) {
-            Block explosionBarrel = (Block) explosionBarrels.get(uuid).stream().filter(b -> b.getLocation() == block.getLocation());
-            explosionBarrels.get(uuid).remove(explosionBarrel);
+            UUID explosionBarrelOwner = (UUID) block.getMetadata(KitMetaData.EXPLOSION_BARREL.getKey()).get(0).value();
+
+            if (explosionBarrelOwner != null) {
+
+                Player playerFromUUID = Bukkit.getPlayer(explosionBarrelOwner);
+
+                if (playerFromUUID != null) {
+                    playerFromUUID.sendMessage(Localization.INSTANCE.getMessage("pirat.destroyed",
+                            ImmutableMap.of("location", String.valueOf(explosionBarrelsHolder.get(explosionBarrelOwner).get(0).toString())),
+                            ChatUtils.getPlayerLocale(playerFromUUID)));
+                }
+
+                explosionBarrelsHolder.get(explosionBarrelOwner).remove(block.getLocation());
+                //block.removeMetadata(KitMetaData.EXPLOSION_BARREL.getKey(), KitApi.getInstance().getPlugin());
+            }
         }
+    }
+
+    // Die Pulverfässer explodieren, wenn man versucht sie mit einem Piston zu verschieben
+    @EventHandler
+    public void onPistonExtend(BlockPistonExtendEvent event) {
+        detonateOnPistonMove(event.getBlocks());
+    }
+
+    @EventHandler
+    public void onPistonRetract(BlockPistonRetractEvent event) {
+        detonateOnPistonMove(event.getBlocks());
+    }
+
+    private void detonateOnPistonMove(List<Block> blocks) {
+        for (Block block : blocks) {
+            UUID explosionBarrelOwner = (UUID) block.getMetadata(KitMetaData.EXPLOSION_BARREL.getKey()).get(0).value();
+            if (explosionBarrelOwner != null) {
+                Player playerFromUUID = Bukkit.getPlayer(explosionBarrelOwner);
+                if (playerFromUUID != null) {
+                    playerFromUUID.sendMessage(Localization.INSTANCE.getMessage("pirat.destroyedbypiston",
+                            ImmutableMap.of("location", String.valueOf(explosionBarrelsHolder.get(explosionBarrelOwner).get(0).toString())),
+                            ChatUtils.getPlayerLocale(playerFromUUID)));
+                }
+                detonateExplosionBarrel(block.getWorld(), explosionBarrelOwner);
+            } else
+                Logger.debug("[Kit-Pirat] Oh no, explosionBarrelOwner bei detonateOnPistonMove war null! Bitte gebe einem Coder bescheid! XD");
+        }
+    }
+
+    private boolean isCanon(ItemStack itemStack) {
+        return itemStack.getItemMeta().getPersistentDataContainer().has(canonKey, PersistentDataType.BYTE);
     }
 
     private boolean isExplosionBarrel(Block block) {
         return block.hasMetadata(KitMetaData.EXPLOSION_BARREL.getKey());
+    }
+
+    private boolean isRemoteDetonator(ItemStack itemStack) {
+        return itemStack.getItemMeta().getPersistentDataContainer().has(remoteDetonatorKey, PersistentDataType.BYTE);
     }
 
     private NamespacedKey markerKeyKitPirat(String key) {
