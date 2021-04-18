63768c91be1c2c073a73b801afb2a9aac8cdb055
Krxwallo <ken.grill@gmx.de>
2021-04-18 13:57:53 +0200

    Add Hemomancer kit

--- src/main/java/de/hglabor/plugins/kitapi/KitApi.java
+++ src/main/java/de/hglabor/plugins/kitapi/KitApi.java
@@ -163,6 +163,7 @@ public final class KitApi {
         kits.add(PoseidonKit.INSTANCE);
         kits.add(PhantomKit.INSTANCE);
         kits.add(ArcherKit.INSTANCE);
+        kits.add(HemomancerKit.INSTANCE);
         //kits.add(PirateKit.INSTANCE);
         //kits.add(BeamKit.INSTANCE);
         //sort alphabetically
