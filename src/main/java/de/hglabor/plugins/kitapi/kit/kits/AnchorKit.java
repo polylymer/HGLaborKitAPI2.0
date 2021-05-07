00b98f55d4d1269869bdbac360a4dd46c4c05c23
Hotkeyyy <64286456+Hotkeyyy@users.noreply.github.com>
2021-05-08 00:46:45 +0200

    Update AnchorKit.java

--- src/main/java/de/hglabor/plugins/kitapi/kit/kits/AnchorKit.java
+++ src/main/java/de/hglabor/plugins/kitapi/kit/kits/AnchorKit.java
@@ -3,64 +3,56 @@ package de.hglabor.plugins.kitapi.kit.kits;
 import de.hglabor.plugins.kitapi.KitApi;
 import de.hglabor.plugins.kitapi.kit.AbstractKit;
 import de.hglabor.plugins.kitapi.kit.events.KitEvent;
+import de.hglabor.plugins.kitapi.kit.settings.IntArg;
 import de.hglabor.plugins.kitapi.kit.settings.SoundArg;
 import de.hglabor.plugins.kitapi.player.KitPlayer;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.Sound;
-import org.bukkit.attribute.Attribute;
-import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.util.Vector;
 
-/**
- * @author Hotkeyyy
- * @since 2021/02/25
- */
 public class AnchorKit extends AbstractKit {
     public static final AnchorKit INSTANCE = new AnchorKit();
     @SoundArg
     private final Sound hitSound;
+    //0 = false, 1 = true (weil boolarg nicht funktioniert)
+    @IntArg(min = 0, max = 1)
+    private final int setVelocity;
 
     private AnchorKit() {
         super("Anchor", Material.ANVIL);
         hitSound = Sound.BLOCK_ANVIL_HIT;
-    }
-
-    @Override
-    public void onDeactivation(KitPlayer kitPlayer) {
-        kitPlayer.getBukkitPlayer().ifPresent(this::resetKnockbackAttribute);
-    }
-
-    @Override
-    public void onEnable(KitPlayer kitPlayer) {
-        kitPlayer.getBukkitPlayer().ifPresent(this::setKnockbackAttribute);
+        setVelocity = 1;
     }
 
     @KitEvent
     @Override
     public void onPlayerAttacksLivingEntity(EntityDamageByEntityEvent event, KitPlayer attacker, LivingEntity entity) {
-        if (entity instanceof Player) {
-            ((Player) entity).playSound(entity.getLocation(), hitSound, 1, 1);
-        }
-        attacker.getBukkitPlayer().ifPresent(player -> player.playSound(player.getLocation(), hitSound, 1, 1));
-        setKnockbackAttribute(entity);
-        Bukkit.getScheduler().runTaskLater(KitApi.getInstance().getPlugin(), () -> resetKnockbackAttribute(entity), 1);
+        handleAnchor(event);
     }
 
-    private void setKnockbackAttribute(LivingEntity entity) {
-        AttributeInstance attribute = entity.getAttribute(Attribute.GENERIC_KNOCKBACK_RESISTANCE);
-        if (attribute != null) {
-            attribute.setBaseValue(1.0);
-        }
+    @KitEvent
+    @Override
+    public void onPlayerGetsAttackedByLivingEntity(EntityDamageByEntityEvent event, Player player, LivingEntity attacker) {
+        handleAnchor(event);
     }
 
-    private void resetKnockbackAttribute(LivingEntity entity) {
-        AttributeInstance attribute = entity.getAttribute(Attribute.GENERIC_KNOCKBACK_RESISTANCE);
-        if (attribute != null) {
-            attribute.setBaseValue(0.0);
+    private void handleAnchor(EntityDamageByEntityEvent event) {
+        if (!(event.getEntity() instanceof LivingEntity)) return;
+        LivingEntity entity = (LivingEntity) event.getEntity();
+        if (setVelocity == 0) {
+            event.setCancelled(true);
+            entity.damage(event.getFinalDamage());
+        } else {
+            Bukkit.getScheduler().runTaskLater(KitApi.getInstance().getPlugin(), () -> {
+                entity.setVelocity(new Vector(0, 0, 0));
+            }, 1L);
         }
+
     }
+
 }
 
